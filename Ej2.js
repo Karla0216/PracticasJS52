8f444e413ba1542cc3410d635a1dad743d1a6f2a
Karla Mayela Rodriguez Garcia <krodriguezgi105@uanl.edu.mx>
2022-10-12 10:10:23 -0500

    ej2

---
+++ Ej2.js
@@ -0,0 +1,14 @@
+/* 2.- Escribir un programa que imprima los horarios
+        en que debe tomar los medicamentos un paciente. 
+        Solicitar al usuario la hora de la toma inicial
+        (0-23) y la frecuencia de la toma diaria (1-12).
+        
+        Ejemplo
+        hora inicial: 5
+        frecuencia: 6
+        horarioss
+        toma 1 5
+        toma 2 11
+        toma 317
+        toma 4 23*/
+
