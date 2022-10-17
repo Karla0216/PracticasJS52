688a7bec115665aca08a3bad8e63ba150afee7ad
Karla Mayela Rodriguez Garcia <krodriguezgi105@uanl.edu.mx>
2022-10-17 09:23:06 -0500

    funcion const  divisores

--- js/js4.js
+++ js/js4.js
@@ -8,10 +8,26 @@ function divisores(numero){
         }
     }
     return total;
-}
+};
 
 /* la funcion dice cuantos divisores tiene
  % es el residuo de una division */
- 
 
-console.log("Total de divisores",divisores(32))
+
+console.log("Total de divisores",divisores(32));
+
+
+/* funcion anonima */
+
+const divisoresV2 = function(numero){
+    let total = 0;
+
+    for(let i=1; i<=numero; i++){
+        if(numero % i == 0){
+            total++;
+        }
+    }
+    return total
+};
+
+console.log("Total de divisores",divisoresV2(32));
