67eb82c8467fbe9a5d892efaea2adf822be40131
Karla Mayela Rodriguez Garcia <krodriguezgi105@uanl.edu.mx>
2022-10-19 11:09:00 -0500

    tabla

--- js/js6.js
+++ js/js6.js
@@ -4,3 +4,18 @@ Tipo ___________
 Longitud _______
 {resultado aqui} 
 <Ver tabla>*/
+
+
+function tabla(){
+    let numero_tabla = parseInt(document.getElementById("tabla").value);
+    let longitud = parseInt(document.getElementById("longitud").value);
+    let tipo = document.getElementById("tipo").value;
+
+    for(let i = 1; i <= longitud; i++){
+        document.write("<li>");
+		document.write(j + "x " + i + "= " + j * i);
+		document.write("</li>");
+    }
+}
+
+
