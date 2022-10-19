ea40c25df46e3f3866bcec9b994b5f578c5bb99a
Karla Mayela Rodriguez Garcia <krodriguezgi105@uanl.edu.mx>
2022-10-19 10:30:40 -0500

    clase del 12 de oct 22

---
+++ js/Ej2.js
@@ -0,0 +1,26 @@
+/* 2.- Escribir un programa que imprima los horarios
+        en que debe tomar los medicamentos un paciente. 
+        Solicitar al usuario la hora de la toma inicial
+        (0-23) y la frecuencia de la toma diaria (1-12).
+        
+        Ejemplo
+        hora inicial: 5
+        frecuencia: 6
+        horarioss
+        toma 1 5
+        toma 2 11
+        toma 3 17
+        toma 4 23
+*/
+
+let horas = 4;
+let hrainicial = 5;
+let frecuencia = 6;
+let toma = 24/frecuencia;
+
+console.log("No. de toma:" , toma);
+
+/* for(let i=hrainicial; i=toma; i=hrainicial){
+        console.log("No. de toma:" , toma, "Hora: ", i);
+} */;
+
