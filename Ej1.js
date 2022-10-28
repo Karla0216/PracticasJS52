082e4707f1cc4b091c6ae97673e5f33b59b98dcc
Karla Mayela Rodriguez Garcia <krodriguezgi105@uanl.edu.mx>
2022-10-28 17:04:31 -0500

    terminado y diseño

--- Ej1.js
+++
@@ -1,6 +0,0 @@
-/* 1.- Escribir un programa em java que permita
-imprimir en pantalla n numeros perfectos. El usuario podra indicar cuantos
-numeros perfectos es aquel que la suma de sus divisiones excepto el mismo,
-da como resultado el mismo numero.
-Ejemplo: para el 6 sus divisiones son 1,2,3,6 
-Sumando todos excepto el 6: 1+2+3= 6, 6 es perfecto. */
