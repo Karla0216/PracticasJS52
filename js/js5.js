94186a423827e0fabb0f958483d3df04d32a6caa
Karla Mayela Rodriguez Garcia <krodriguezgi105@uanl.edu.mx>
2022-10-28 19:24:56 -0500

    funciones terminadas

--- js/js5.js
+++ js/js5.js
@@ -27,4 +27,35 @@ function calcular(){
     let resul = document.getElementById("resultado");
     resul.value = resultado;
 
-}
+};
+
+function Tabla() {
+    let numtabla = parseInt(document.getElementById("tabla").value);
+    let tipo = parseInt(document.getElementById("tipo").value);
+    let longitud = parseInt(document.getElementById("long").value);
+    let tabla = document.getElementById("tabla-resul");
+    let push = document.createDocumentFragment();
+
+    document.getElementById("tablanum").textContent = `Tabla del ${numtabla}`;
+    
+    switch (tipo) {
+        case 1:
+            for (let i = 0; i <= longitud; i++) {
+                let renglon = document.createElement("tr")
+                renglon.innerHTML = `<td>${numtabla}</td><td>x</td><td>${i}</td><td>=</td><td>${(numtabla * i)}</td>`;
+                push.appendChild(renglon);
+            };
+            tabla.innerHTML = "";
+            tabla.appendChild(push);
+            break;
+        case 2:
+            for (let i = longitud; i >= 0; i--) {
+                let renglon = document.createElement("tr")
+                renglon.innerHTML = `<td>${numtabla}</td><td>x</td><td>${i}</td><td>=</td><td>${(numtabla * i)}</td>`;
+                push.appendChild(renglon);
+            };
+            tabla.innerHTML = "";
+            tabla.appendChild(push);
+            break;
+    };
+};
